8bf50834c92aada07da37565dd22fd3fd53ba290
jaafaralireeabi <159414760+jaafaralireeabi@users.noreply.github.com>
2026-02-08 15:08:13 +0300

    Delete lab2/exmaples/example2.py

--- lab2/exmaples/example2.py
+++
@@ -1,7 +0,0 @@
-# Read two numbers from user
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-# Addition operation
-sum_result = num1 + num2
-# Display result
-print("The sum is:", sum_result)
